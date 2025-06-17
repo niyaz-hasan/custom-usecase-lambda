48573b868cd1059ca36f0a028053e6178a59dd4b
niyaz-hasan <60500585+niyaz-hasan@users.noreply.github.com>
2025-06-17 10:14:06 +0530

    added lambda github workflow

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
- # terraform {
- # backend "s3" {
- #   bucket = "terraform-remote-st"
- #   key    = "lambda/terraform.tfstate"
- #   region = "us-east-1" 
- #
- #   # For State Locking
- #   dynamodb_table = "terraform-locks"    
- # } 
- # } 
+  terraform {
+  backend "s3" {
+    bucket = "terraform-remote-st"
+    key    = "lambda-unused/terraform.tfstate"
+    region = "us-east-1" 
+ 
+    # For State Locking
+    dynamodb_table = "terraform-locks"    
+  } 
+  } 
